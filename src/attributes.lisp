f151c313db484d0618b2055e4549feb5f36e761c
Andy Best <andybest.net@gmail.com>
2022-01-03 13:17:13 +0000

    Null terminated array WIP

--- src/attributes.lisp
+++ src/attributes.lisp
@@ -1,5 +1,6 @@
 (in-package :openexr.attributes)
 
+
 (defbinary box2i (:export t :byte-order :little-endian)
            (xmin 0 :type (signed-byte 32))
            (ymin 0 :type (signed-byte 32))
@@ -26,12 +27,33 @@
            (ysampling 0 :type (signed-byte 32)))
 
 
-(defbinary openexr-header-attribute (:export t :byte-order :little-endian)
-  (name "" :type (terminated-string 1 :terminator 0))
-  (attribute-type "" :type (terminated-string 1 :terminator 0))
-  (size 0 :type (signed-byte 32))
-  (value 0 :type (eval
-                  (case attribute-type
-                    ("box2i" 'box2i)
-                    ("box2f" 'box2f)
-                    ("chlist" '())))))
+;; (defbinary openexr-header-attribute (:export t :byte-order :little-endian)
+;;   (name "" :type (terminated-string 1 :terminator 0))
+;;   (attribute-type "" :type (terminated-string 1 :terminator 0))
+;;   (size 0 :type (signed-byte 32))
+;;   (value 0 :type (eval
+;;                   (case attribute-type
+;;                     ("box2i" 'box2i)
+;;                     ("box2f" 'box2f)
+;;                     ("chlist" '())))))
+
+
+(define-lisp-binary-type type-info (counted-array count-size element-type &key bind-index-to)
+  :where (eq counted-array 'counted-array)
+  (let ((count-size* (gensym "COUNT-SIZE-")))
+    	(reader-value  (gensym "READER-VALUE-"))
+    	(reader-byte-count (gensym "READER-BYTE-COUNT-")
+        (letf (((slot-value type-info 'type))))
+    	    `(simple-array ,element-type ((read-integer ,count-size* ,stream-symbol :byte-order ,byte-order))
+           ,@(if bind-index-to)
+           `(:bind-index-to ,bind-index-to
+                (multiple-value-bind (defstruct-type reader writer))))
+    	  (expand-defbinary-type-field type-info))
+    	(setf writer `(let ((,count-size* ,count-size)))
+              			(+)
+               (write-integer (length ,name) ,count-size* ,stream-symbol :byte-order ,byte-order)
+               ,writer)
+    	(setf reader `(let ((,count-size* ,count-size)))
+              			(multiple-value-bind (,reader-value ,reader-byte-count) ,reader)
+                (values ,reader-value (+ ,count-size* ,reader-byte-count)))
+    	(values (getf defstruct-type :type) reader writer)))
